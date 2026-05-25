461073f2bdb68f12eed14c95e8d185a9d33ac707
Jacob <juraboev0115@naver.com>
2026-05-25 17:24:06 +0900

    feat: mtask

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,18 @@
-console.log("Heloo World!");
+function getSquareNumbers(numbers: number[]) {
+    const result = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const obj = {
+            number: numbers[i],
+            square: numbers[i] * numbers[i],
+        };
+
+        result.push(obj);
+    }
+
+    return result;
+}
+
+const answer = getSquareNumbers([4, 5, 6, 7]);
+
+console.log(answer);
