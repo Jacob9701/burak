deeb74a072998fda70066639be14c6d038f9092a
Jacob <juraboev0115@naver.com>
2026-05-29 18:13:23 +0900

    feat: member service model integration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 import app from "./app";
 
@@ -8,9 +7,7 @@ mongoose
     .connect(process.env.MONGO_URL as string, {})
     .then((data) => {
         console.log("MongoDB connection succeed");
-
         const PORT = process.env.PORT ?? 3003;
-
         app.listen(PORT, function () {
             console.log(`The server is running successfully on port: ${PORT}`);
         });
