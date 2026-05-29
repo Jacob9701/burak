2e0c8aebdc3c01c3f508656de8a4355aee1d7c68
Jacob <juraboev0115@naver.com>
2026-05-29 17:53:58 +0900

    fix: reform routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE **/
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/", router);
+app.use("/admin", routerAdmin); // EJS
+app.use("/", router); // REACT
 
 export default app;
