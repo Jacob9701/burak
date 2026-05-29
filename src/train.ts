32b6cdf0aa26cfc1f55bc963cf3406aa2ee94a11
Jacob <juraboev0115@naver.com>
2026-05-29 20:01:39 +0900

    Added square numbers and palindrome check

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,39 @@
 function getSquareNumbers(numbers: number[]) {
-    const result = [];
+  const result = [];
 
-    for (let i = 0; i < numbers.length; i++) {
-        const obj = {
-            number: numbers[i],
-            square: numbers[i] * numbers[i],
-        };
+  for (let i = 0; i < numbers.length; i++) {
+    const obj = {
+      number: numbers[i],
+      square: numbers[i] * numbers[i],
+    };
 
-        result.push(obj);
-    }
+    result.push(obj);
+  }
 
-    return result;
+  return result;
 }
 
 const answer = getSquareNumbers([4, 5, 6, 7]);
 
 console.log(answer);
+
+
+
+
+
+function palindromCheck(word: string): boolean {
+  let reversed = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+
+  if (word === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(palindromCheck("non")); // true
+console.log(palindromCheck("kitob")); // false
