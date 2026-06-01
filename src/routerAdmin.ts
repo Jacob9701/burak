83f4c29e2aea2d7b4da3941a768575c7f90dd123
Jacob <juraboev0115@naver.com>
2026-06-01 15:33:39 +0900

    fix routerAdmin

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,20 +1,26 @@
 import express from "express";
-const routerAdmin = express.Router();
+import path from "path";
+import router from "./router";
+import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
-import restaurantController from "./controllers/restaurant.controller";
+/** 1-ENTRANCE **/
+const app = express();
 
-/** Restaurant */
-routerAdmin.get("/", restaurantController.goHome);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
-routerAdmin
-    .get("/login", restaurantController.getLogin)
-    .post("/login", restaurantController.processLogin);
+/** 2-SESSIONS **/
 
-routerAdmin
-    .get("/signup", restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+/** 3-VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-/** Product */
-/** User */
+/** 4-ROUTERS **/
+app.use("/admin", routerAdmin); // EJS
+app.use("/", router); // REACT
 
-export default routerAdmin;
+export default app;
