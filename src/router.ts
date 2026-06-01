a8c18258e3339f91f166ed493697a73c7cda4f3a
Jacob <juraboev0115@naver.com>
2026-06-01 19:12:08 +0900

    App running

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import express from "express";
+
 const router = express.Router();
-import memberController from "./controllers/restaurant.controller";
 
 export default router;
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,26 +1,12 @@
 import express from "express";
-import path from "path";
-import router from "./router";
-import routerAdmin from "./routerAdmin";
-import morgan from "morgan";
-import { MORGAN_FORMAT } from "./libs/config";
+import restaurantController from "./controllers/restaurant.controller";
 
-/** 1-ENTRANCE **/
-const app = express();
+const routerAdmin = express.Router();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan(MORGAN_FORMAT));
+routerAdmin.get("/", restaurantController.goHome);
+routerAdmin.get("/login", restaurantController.getLogin);
+routerAdmin.get("/signup", restaurantController.getSignup);
+routerAdmin.post("/login", restaurantController.processLogin);
+routerAdmin.post("/signup", restaurantController.processSignup);
 
-/** 2-SESSIONS **/
-
-/** 3-VIEWS **/
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-/** 4-ROUTERS **/
-app.use("/admin", routerAdmin); // EJS
-app.use("/", router); // REACT
-
-export default app;
+export default routerAdmin;
