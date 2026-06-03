3dcf5ca5e4d6bec518505e7a53fe6d4e3ae09d22
Jacob <juraboev0115@naver.com>
2026-06-03 16:13:31 +0900

    feat: P-TASK

--- src/train.ts
+++ src/train.ts
@@ -43,53 +43,75 @@
 
 
 
-export enum HttpCode {
-  OK = 200,
-  CREATED = 201,
-  NOT_MODIFIED = 304,
-  BAD_REQUEST = 400,
-  UNAUTHORIZED = 401,
-  FORBIDDEN = 403,
-  NOT_FOUND = 404,
-  INTERNAL_SERVER_ERROR = 500,
-}
+// export enum HttpCode {
+//   OK = 200,
+//   CREATED = 201,
+//   NOT_MODIFIED = 304,
+//   BAD_REQUEST = 400,
+//   UNAUTHORIZED = 401,
+//   FORBIDDEN = 403,
+//   NOT_FOUND = 404,
+//   INTERNAL_SERVER_ERROR = 500,
+// }
 
-export enum Message {
-  SOMETHING_WENT_WRONG = "Something went wrong!",
-  NO_DATA_FOUND = "No data is found!",
-  CREATE_FAILED = "Create is failed!",
-  UPDATE_FAILED = "Update is failed!",
-}
+// export enum Message {
+//   SOMETHING_WENT_WRONG = "Something went wrong!",
+//   NO_DATA_FOUND = "No data is found!",
+//   CREATE_FAILED = "Create is failed!",
+//   UPDATE_FAILED = "Update is failed!",
+// }
 
-class Errors extends Error {
-  public code: HttpCode;
-  public message: Message;
+// class Errors extends Error {
+//   public code: HttpCode;
+//   public message: Message;
 
-  constructor(statusCode: HttpCode, statusMessage: Message) {
-    super();
-    this.code = statusCode;
-    this.message = statusMessage;
-  }
-}
+//   constructor(statusCode: HttpCode, statusMessage: Message) {
+//     super();
+//     this.code = statusCode;
+//     this.message = statusMessage;
+//   }
+// }
 
-export default Errors;
+// export default Errors;
 
 
 
 //O-TASK
 
-function calculateSumOfNumbers(arr: any[]): number {
-  let sum = 0;
+// function calculateSumOfNumbers(arr: any[]): number {
+//   let sum = 0;
 
-  for (let item of arr) {
-    if (typeof item === "number") {
-      sum += item;
+//   for (let item of arr) {
+//     if (typeof item === "number") {
+//       sum += item;
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(
+//   calculateSumOfNumbers([5, "hello", false, { age: 20 }, 15, null, 25])
+// );
+
+
+function objectToArray(obj: { [key: string]: any }): [string, any][] {
+  const result: [string, any][] = [];
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push([key, obj[key]]);
     }
   }
 
-  return sum;
+  return result;
 }
 
-console.log(
-  calculateSumOfNumbers([5, "hello", false, { age: 20 }, 15, null, 25])
-);
+const laptop = {
+  brand: "Apple",
+  model: "MacBook Air",
+  year: 2024,
+  price: 1500,
+};
+
+console.log(objectToArray(laptop));
