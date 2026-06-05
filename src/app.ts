fd76e0758613bb722194812fc285ab4ea0ab943b
Jacob <juraboev0115@naver.com>
2026-06-05 18:56:33 +0900

    fix: organize spa & ssr business logic

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import { MORGAN_FORMAT } from "./libs/config";
 
 /** 1-ENTRANCE **/
 const app = express();
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,7 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/admin", routerAdmin); // EJS
-app.use("/", router); // REACT
+app.use("/admin", routerAdmin); // SSR
+app.use("/", router); // SPA
 
 export default app;
