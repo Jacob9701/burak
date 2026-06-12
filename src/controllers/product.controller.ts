c6d39d0cf84a3b53ca1ac515f9c3d02a82df707a
Jacob <juraboev0115@naver.com>
2026-06-12 19:24:16 +0900

    fix: getAllProduct api and passing date into egs

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -15,9 +15,14 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
     try {
         console.log("getAllProducts");
-        res.render("products");
+
+        const data = await productService.getAllProducts();
+        console.log("data:", data);
+
+        res.render("products", { products: data });
     } catch (err) {
         console.log("Error, getAllProducts:", err);
+
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
     }
